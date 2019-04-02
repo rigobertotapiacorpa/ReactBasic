52ad5569a555061a0c32b950bcb4bc9f4f35fb2f
rigobertotapiacorpa <rodrigo3adriana0@gmail.com>
2019-04-02 10:24:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-fex3jn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-fex3jn)
---
+++ index.js
@@ -0,0 +1,158 @@
+import React, { Component , useState} from 'react'; //useState es un hucks
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+//let nombre = "Rigoberto";
+
+/*function A(props){
+  return <p> hola {props.name} </p>;
+}
+function B(props){
+  return <p> {props.name} : 31</p>;
+}
+
+
+class MiComponentClass extends Component{
+  render(){
+    return <p>Hello word Bolivia </p>;
+  }
+}
+
+class Contador extends Component{
+
+   constructor(props){
+     super(props);
+
+     this.state= {
+      contador:0
+     }
+   }
+  contador = ()
+    =>{this.setState
+        ({contador: this.state.contador+1})
+      } 
+  render(){
+    return (
+      <div>
+        <p>{this.state.contador}</p>
+        <button onClick = {this.contador}>Aumentar</button>
+      </div>
+    )
+  }
+}
+*/
+function Contador(props){
+  const[contador, setContador] = useState(0);
+  return(
+    <div>
+      <p>conteo:{contador}</p>
+      <button onClick={()=>setContador(contador+1)}>Incrementar</button>
+    </div>
+  )
+}
+ class Formulario extends Component{
+   constructor(props){
+     super(props);
+
+     this.state={
+       email:'',
+       password:''
+     }
+   }
+  
+  sincronizarCambios(value,property){
+    let state ={};
+
+    state[property]=value;
+    this.setState(state);
+  }
+  
+  submitForm = ()
+    =>{console.log(this.state)
+      } 
+
+   render(){
+     return(
+      <form>
+        <input 
+          onChange={(event)=>{this.sincronizarCambios(event.target.value, 'email')}}
+          type="email"
+          value={this.state.email}
+          placeholder="email"/>        
+        <input
+          onChange={(event)=>{this.sincronizarCambios(event.target.value, 'password')}}
+          type="password"
+          value={this.state.password}
+          placeholder="******"/>
+        <div>
+          <input
+            onClick={this.submitForm}
+            type="submit"
+            value="Iniciar Sesión"/>
+        </div>
+      </form>
+     )
+   }
+ }
+ //  ARREGLOS
+ class Arreglo extends Component{
+   constructor(props){
+     super(props);
+
+     this.state={
+       articles:[]
+     }
+   }
+
+   componentDidMount(){
+     let promesa = fetch('https://jsonplaceholder.typicode.com/posts');
+
+      promesa.then(response=> response.json()).then((data)=>{
+        // console.log(data);
+        this.setState({
+          articles:data
+        })
+      })
+     
+   }
+   
+   render(){
+     return(
+       <div> {this.state.articles.map((article)=>{
+                return <div className="card" style={{ backgroundColor: 'red',color:'yellow'}}><p>{article.title}</p></div>
+                      
+                })
+              }
+       </div>
+     )
+   }
+ }
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+
+render(){
+  let nombre = "Rigoberto";
+    return(
+      <div>
+        < Formulario/>
+        <Contador/>
+        <Arreglo/>
+        
+    
+      </div>
+    )
+  };  
+  /*  // <A name = {nombre}/>
+       // <B name = {nombre}/>
+       // < Contador/> */
+  }
+
+render(<App />, document.getElementById('root'));
